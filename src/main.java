31036fa6ed57ad26864e32ca6e39cc156cc66875
aaminter <aam.interpolyaris@gmail.com>
2025-03-22 11:18:42 +0300

    Initial Commit

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание 7: условный оператор. Часть 2");
+        System.out.println();
+    }
+}
